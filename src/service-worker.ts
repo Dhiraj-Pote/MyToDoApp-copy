f1c2d1f636371537f8ce04ee55ca90f8cda2c3f4
Dhiraj-Pote <dhirajpote29@gmail.com>
2023-07-27 17:35:24 +0530

    final

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -60,14 +60,14 @@ registerRoute(
   ({ url }) =>
     url.origin === self.location.origin && url.pathname.endsWith(".png"),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
-    ],
-  })
+  // new StaleWhileRevalidate({
+  //   cacheName: "images",
+  //   plugins: [
+  //     // Ensure that once this runtime cache reaches a maximum size the
+  //     // least-recently used images are removed.
+  //     new ExpirationPlugin({ maxEntries: 50 }),
+  //   ],
+  // })
 );
 
 // This allows the web app to trigger skipWaiting via
