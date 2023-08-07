e191129ec52689b345f0a4165a3ec01c657168a4
Dhiraj-Pote <dhirajpote29@gmail.com>
2023-08-07 07:42:12 +0530

    7aug

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,59 +12,11 @@ const About = () => {
     <>
       <Container>
         <p style={{ fontSize: "1.3rem" }}>
-          <b>This is Raina's Personal Todo App</b> gifted by Shristi with love. 
+          <b>This is Raina's Personal Todo App</b> gifted by Shristi with love.
           So, dear Raina Write your todos, complete the tasks,
           change some settings, Enjoy!
         </p>
-        <h2>Contact</h2>
-        <a
-          href="https://github.com/Dhiraj-Pote"
-          style={{
-            color: "#24292E",
-            textDecoration: "none",
-            fontSize: "24px",
-          }}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <GitHub fontSize="large" />
-        </a>
-        <a
-          href="https://www.facebook.com/dhiraj.pote.589"
-          style={{
-            color: "#0D8BF0",
-            textDecoration: "none",
-            fontSize: "24px",
-          }}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Facebook fontSize="large" />
-        </a>
-        <a
-          href="https://www.youtube.com/kcclips"
-          style={{
-            color: "#FD0000",
-            textDecoration: "none",
-            fontSize: "24px",
-          }}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <YouTube fontSize="large" />
-        </a>
-        <a
-          href="https://www.instagram.com/ys_dhiraj/"
-          style={{
-            color: "#CD2E96",
-            textDecoration: "none",
-            fontSize: "24px",
-          }}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Instagram fontSize="large" />
-        </a>
+
       </Container>
     </>
   );
