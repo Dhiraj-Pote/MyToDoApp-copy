80920b7a808cf8000ca5bd79d47b641fde6ed843
Dhiraj-Pote <dhirajpote29@gmail.com>
2023-08-07 17:23:47 +0530

    7aug

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,7 @@ function App() {
   const { addTodo } = useContext(MainContext)!;
 
   const images = [
-    "https://live.staticflickr.com/65535/53098885669_426b857606_n.jpg",
-    "https://live.staticflickr.com/65535/53098706701_10649a54b6_w.jpg",
     "https://live.staticflickr.com/65535/53098885489_bd1c7fbdd7_n.jpg",
-    "https://live.staticflickr.com/65535/53098141447_4a64a91815_w.jpg",
-    "https://live.staticflickr.com/65535/53098130212_e564c775f6_m.jpg",
-    "https://live.staticflickr.com/65535/53099197098_886d750c51_m.jpg",
-    "https://live.staticflickr.com/65535/53098885494_a521b73887_n.jpg",
-    "https://live.staticflickr.com/65535/53098885514_aa5aaca046_n.jpg",
-    "https://live.staticflickr.com/65535/53099203138_1f89c3378b_w.jpg",
     "https://live.staticflickr.com/65535/53098885669_426b857606_n.jpg",
     "https://live.staticflickr.com/65535/53099107880_6d2aa82b3a_n.jpg",
     "https://live.staticflickr.com/65535/53098885479_c52d4a29c1_w.jpg",
@@ -29,6 +21,15 @@ function App() {
     "https://live.staticflickr.com/65535/53098885694_7ef3dde028_n.jpg",
     "https://live.staticflickr.com/65535/53099203018_348227aac3_w.jpg",
     "https://live.staticflickr.com/65535/53099203003_b41df8db74_w.jpg",
+    "https://live.staticflickr.com/65535/53098706701_10649a54b6_w.jpg",
+    "https://live.staticflickr.com/65535/53098141447_4a64a91815_w.jpg",
+    "https://live.staticflickr.com/65535/53098130212_e564c775f6_m.jpg",
+    "https://live.staticflickr.com/65535/53099197098_886d750c51_m.jpg",
+    "https://live.staticflickr.com/65535/53099203043_3de5d25086_w.jpg",
+    "https://live.staticflickr.com/65535/53098885494_a521b73887_n.jpg",
+    "https://live.staticflickr.com/65535/53098885514_aa5aaca046_n.jpg",
+    "https://live.staticflickr.com/65535/53099203138_1f89c3378b_w.jpg",
+    "https://live.staticflickr.com/65535/53098706461_73746f79ac_z.jpg",
     "https://live.staticflickr.com/65535/53099203048_ff208ec54c_n.jpg",
     "https://live.staticflickr.com/65535/53098141452_5a6c5be8a7_w.jpg"
   ];
@@ -46,9 +47,7 @@ function App() {
       <Route path="/about">
         <About />
       </Route>
-        <BackgroundSlideshow images={images} />
-      
-      
+      <BackgroundSlideshow images={images} />
     </div>
   );
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,11 +12,59 @@ const About = () => {
     <>
       <Container>
         <p style={{ fontSize: "1.3rem" }}>
-          <b>This is Raina's Personal Todo App</b> gifted by Shristi with love.
-          So, dear Raina Write your todos, complete the tasks,
-          change some settings, Enjoy!
+          <b>This is Raina's Personal website</b> a small gift made by Dhiraj with lot's of love.
+          So Raina write your todos,change some settings, Enjoy! <br/>
+          Date: 06/08/23
         </p>
-
+        <h2>Contact</h2>
+        <a
+          href="https://github.com/Dhiraj-Pote"
+          style={{
+            color: "#24292E",
+            textDecoration: "none",
+            fontSize: "24px",
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub fontSize="large" />
+        </a>
+        <a
+          href="https://www.facebook.com/dhiraj.pote.589"
+          style={{
+            color: "#0D8BF0",
+            textDecoration: "none",
+            fontSize: "24px",
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Facebook fontSize="large" />
+        </a>
+        <a
+          href="https://www.youtube.com/kcclips"
+          style={{
+            color: "#FD0000",
+            textDecoration: "none",
+            fontSize: "24px",
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <YouTube fontSize="large" />
+        </a>
+        <a
+          href="https://www.instagram.com/ys_dhiraj/"
+          style={{
+            color: "#CD2E96",
+            textDecoration: "none",
+            fontSize: "24px",
+          }}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Instagram fontSize="large" />
+        </a>
       </Container>
     </>
   );
